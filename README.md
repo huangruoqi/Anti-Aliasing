9fe39824a9aba3e75b5c50fbaddd57acc7f2c4c0
Ruoqi Huang <huangruoqi374@gmail.com>
2022-11-01 17:14:49 -0700

    first commit

--- README.md
+++ README.md
@@ -1,12 +1,11 @@
-# Rust *wgpu* Graphics Programming: Step-By-Step (2) 
+# Rust Anti-Aliasing Line
 
-## Set Up Development Environment 
+Install Rust: https://www.rust-lang.org/tools/install
+Execute the following command in terminal
+* `cargo run`
 
-This is the source code for second video of the YouTube series on step-by-step Rust *wgpu* graphics programming.
+Library:
+* using ferrux_canvas currently, open to other Canvas implementations
 
-This sample *wgpu* app shows how to set up the development environment for building *wgpu* apps in Rust.
 
-## Link for All Source Code Used in the Rust *wgpu* Step-By-Step Video Series:
-
-https://github.com/jack1232/wgpu-Step-By-Step
 
--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,60 @@
-use winit::{
-    event::{Event, WindowEvent},
-    event_loop::{ControlFlow, EventLoop},
-    window::Window,
-};
+use winit::dpi::LogicalSize;
+use winit::event::{Event, WindowEvent};
+use winit::event_loop::{ControlFlow, EventLoop};
+use winit::window::WindowBuilder;
+use ferrux_canvas::canvas::Canvas;
+use ferrux_canvas::canvas::winit::WinitCanvas;
+use ferrux_canvas::color::{ColorBuilder, palette};
 
 fn main() {
-    let instances = wgpu::Instance::new(wgpu::Backends::all());
-    for adapter in instances.enumerate_adapters(wgpu::Backends::all()) {
-        println!("{:?}", adapter.get_info())
-    }
-
     let event_loop = EventLoop::new();
-    let window = Window::new(&event_loop).unwrap();
-    window.set_title("my window");
-    env_logger::init();
+    let window = {
+        let size = LogicalSize::new(960, 480);
+        WindowBuilder::new()
+          .with_title("FerruX Canvas")
+          .with_inner_size(size)
+          .with_min_inner_size(size)
+          .build(&event_loop)
+          .unwrap()
+    };
+    let mut canvas = WinitCanvas::new(&window).unwrap();
+    let mut x: i32 = 1;
+    let mut incrementing = true;
 
     event_loop.run(move |event, _, control_flow| {
-        *control_flow = ControlFlow::Wait;
         match event {
             Event::WindowEvent {
                 event: WindowEvent::CloseRequested,
                 ..
-            } => *control_flow = ControlFlow::Exit,
-            _ => {}
+            } => {
+                *control_flow = ControlFlow::Exit
+            }
+            Event::MainEventsCleared => {
+                window.request_redraw();
+            }
+            Event::RedrawRequested(_) => {
+                if !(1..100).contains(&x) {
+                    incrementing = !incrementing;
+                }
+                x += if incrementing { 1 } else { -1 };
+
+                let a = 200 - x as u32;
+
+                canvas.fill_triangle((100, 100 + x as u32), (100 + x as u32, 100),
+                                     (200 + x as u32, 200 + x as u32),
+                ColorBuilder::new().with_red(125).with_blue(150).with_green(50).build());
+
+                canvas.draw_triangle((100, 100 - x as u32), (100 - x as u32, 100),
+                                     (a, a), palette::WHITE);
+
+                canvas.draw_line((a, 100), (a, a), palette::RED);
+                canvas.draw_line((a, a), (100, a), palette::BLUE);
+                canvas.draw_line((100, a), (a, 100), palette::GREEN);
+
+                canvas.render().unwrap();
+                canvas.reset_frame();
+            }
+            _ => (),
         }
     });
 }
